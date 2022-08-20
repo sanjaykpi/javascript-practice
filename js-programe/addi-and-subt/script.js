19f2a97a0aed8e4d8fbcf3843a83fb08fa5c055c
Sanjay Selvamani <sanjayselvamani6@gmail.com>
2022-08-20 12:12:10 -0700

    js-pro

--- js-programe/addi-and-subt/script.js
+++ js-programe/addi-and-subt/script.js
@@ -1,14 +1,55 @@
-    //    addition
+   //    addition
 
-  let a,b,c;
-  a=10,b=20;
-  c=a+b
-  console.log(c)
+  //let a,b,c;
+  //a=10,b=20;
+  //c=a+b
+  //console.log(c)
 
 
     //  subtraction
 
- let x,y,z   
- x=30,y=15;
- z=x-y;
- console.log(z)
+ //let x,y,z   
+ //x=30,y=15;
+ //z=x-y;
+ //console.log(z)
+ 
+ 
+ 
+ //let mrk1=parseInt(prompt("Enter the first mark"));
+ //let mrk2=parseInt(prompt("Enter the secound mark"));
+ //let mrk3=parseInt(prompt("Enter the third mark"));
+ 
+// if (mrk1<35){
+	//console.log("The given mark 1 is a FAIL grade");
+	//if (mrk2<35)
+	//		console.log("The given mark 2 is a FAIL grade",mrk2);
+	//else
+		//	console.log("The given mark 2 is a PASS grade",mrk2);
+		
+	//if (mrk3<35)
+		//	console.log("The given mark 3 is a FAIL grade",mrk3);
+	//else
+		//	console.log("The given mark 3 is a PASS grade",mrk3);
+ //}	
+// else{
+//	console.log("The given mark 1 is a PASS grade",mrk1); 
+//	if (mrk3<35)
+//			console.log("The given mark 3 is a FAIL grade",mrk3);
+//	else
+//			console.log("The given mark 3 is a PASS grade",mrk3);
+// 
+//	if (mrk2<35)
+//			console.log("The given mark 2 is a FAIL grade",mrk2);
+//	else
+//			console.log("The given mark 2 is a PASS grade",mrk2);
+//}
+
+
+let a=parseInt(prompt("Enter the First number"));
+let b=parseInt(prompt("Enter the Secound number"));
+if (a==b){
+	console.log("Both the Given numbers are EQUAL");
+}else if (a<b)
+	console.log("The Secound given number is greater",b);
+else 
+	console.log("The First given number is greater",a);
---
+++ js-programe/factorial-series/script.js
@@ -0,0 +1,14 @@
+
+let a,b;
+
+a=1;
+
+b=(parseInt(prompt("enter the number")));
+
+for(i=1;i<=b;i++){
+
+    a *=i;
+
+    console.log(a);
+
+}
